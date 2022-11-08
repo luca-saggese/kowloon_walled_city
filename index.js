7a0ae530896b7ef606d5b3535c4cc760e3aa9d45
Luca Saggese <luca.saggese@gmail.com>
2022-11-08 17:10:50 +0100

    wip

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const PORT = 8088;
 
-const host = 'http://localhost:' + PORT;
+const host = 'http://www.thingsmind.com:' + PORT;
 
 app.get('/getData.php', (req, res) => {
     const filename = `./json/${req.query['f']}${req.query['chapter'] ? '_' + req.query['chapter'] : ''}.json`;
@@ -21,6 +21,7 @@ app.get('/api-video/find_all_videos.asp', (req, res) => {
 
 app.use(express.static(path.join(__dirname, 'kwc')));
 
-app.listen(PORT, '0.0.0.0');
+app.listen(PORT, '0.0.0.0',()=>{
+  console.log('open ' + host + '/index.html')  
+});
 
-console.log('open ' + host + '/index.html')
